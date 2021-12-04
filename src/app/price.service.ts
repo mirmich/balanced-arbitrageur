2b5c42a7906e1ef9589a0dc454c596cabb9a7fbc
mirmich <michalem95@gmail.com>
2021-12-04 17:14:35 +0100

    1 file added, 1 deleted

--- src/app/price.service.ts
+++ src/app/price.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { PairPrice } from './pair-price';
 
 @Injectable()
-export class PriceServiceService {
+export class PriceService {
   constructor(private http: HttpClient) {}
 
   getPrices() {
