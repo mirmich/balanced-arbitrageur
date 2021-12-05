5e0bcfae465cd232e7f4bf704517995ce840d3bf
mirmich <michalem95@gmail.com>
2021-12-05 15:52:35 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -10,7 +9,7 @@ import { PairListComponent } from './pair-list/pair-list.component';
 import { BotBarComponent } from './bot-bar/bot-bar.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     HelloComponent,
