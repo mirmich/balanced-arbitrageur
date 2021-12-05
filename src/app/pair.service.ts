f99854d8a7498d3d21a099a6d926bd3a7357ea3a
mirmich <michalem95@gmail.com>
2021-12-05 17:37:33 +0100

    3 files modified

--- src/app/pair.service.ts
+++ src/app/pair.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PairPrice } from './pair-price';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +10,13 @@ import { PairPrice } from './pair-price';
 export class PairService {
   constructor(private http: HttpClient) {}
 
+  address: string = 'https://ctz.solidwallet.io/api/v3';
+
   getPrices() {
     return this.http.get<PairPrice[]>('/assets/prices.json');
+  }
+
+  getPricesReal() {
+    return this.http.post(this.address);
   }
 }
