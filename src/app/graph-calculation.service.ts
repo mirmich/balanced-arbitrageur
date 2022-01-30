1841a56abdb2736a04180b44101ccbcac7a1900e
mirmich <michalem95@gmail.com>
2022-01-30 18:26:38 +0100

    2 files modified

--- src/app/graph-calculation.service.ts
+++ src/app/graph-calculation.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import Graph from 'graphology';
+import {dfs} from 'graphology-traversal/dfs';
 
 type NodeType = { name: string };
 
@@ -14,6 +15,7 @@ export class GraphCalculationService {
     graph.addNode('sICX');
     graph.addNode('BALN');
     graph.addNode('bnUSD');
+    graph.addNode('IUSDC');
     // sICX/BALN
     graph.addDirectedEdgeWithKey('sICX->BALN', 'sICX', 'BALN', { price: 0.96 });
     graph.addDirectedEdgeWithKey('BALN->sICX', 'BALN', 'sICX', { price: 1.04 });
@@ -23,8 +25,17 @@ export class GraphCalculationService {
     // bnUSD/BALN
     graph.addDirectedEdgeWithKey('BALN->bnUSD', 'BALN', 'bnUSD', { price: 0.77 });
     graph.addDirectedEdgeWithKey('bnUSD->BALN', 'bnUSD', 'BALN', { price: 1.28 });
+    // bnUSD/IUSDC
+    graph.addDirectedEdgeWithKey('bnUSD->IUSDC', 'bnUSD', 'IUSDC', { price: 1.04 });
+    graph.addDirectedEdgeWithKey('IUSDC->bnUSD', 'IUSDC', 'bnUSD', { price: 1.28 });
+
+    graph.addDirectedEdgeWithKey('IUSDC->sICX', 'IUSDC', 'sICX', { price: 1.26 });
+    graph.addDirectedEdgeWithKey('sICX->IUSDC', 'sICX', 'IUSDC', { price: 0.77 });
     console.log(graph.size);
     console.log(graph.toJSON());
+    dfs(graph, function (node, attr, depth) {
+      console.log(node, attr, depth);
+    });
     // const graph = new Graph<NodeType>((n: NodeType) => n.name);
     // graph.insert({ name: 'sICX' });
     // console.log(graph.getNodes());
