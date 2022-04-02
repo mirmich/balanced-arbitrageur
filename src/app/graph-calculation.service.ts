191c69943e9808fa7d4d6dd8d499a5283442a204
mirmich <michalem95@gmail.com>
2022-04-02 12:15:22 +0200

    2 files modified

--- src/app/graph-calculation.service.ts
+++ src/app/graph-calculation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import Graph from 'graphology';
-import {dfs} from 'graphology-traversal/dfs';
+import { dfs } from 'graphology-traversal/dfs';
+import { allSimplePaths } from 'graphology-simple-path';
 
 type NodeType = { name: string };
 
@@ -20,22 +21,40 @@ export class GraphCalculationService {
     graph.addDirectedEdgeWithKey('sICX->BALN', 'sICX', 'BALN', { price: 0.96 });
     graph.addDirectedEdgeWithKey('BALN->sICX', 'BALN', 'sICX', { price: 1.04 });
     // bnUSD/sICX
-    graph.addDirectedEdgeWithKey('sICX->bnUSD', 'sICX', 'bnUSD', { price: 0.74 });
-    graph.addDirectedEdgeWithKey('bnUSD->sICX', 'bnUSD', 'sICX', { price: 1.32 });
+    graph.addDirectedEdgeWithKey('sICX->bnUSD', 'sICX', 'bnUSD', {
+      price: 0.74,
+    });
+    graph.addDirectedEdgeWithKey('bnUSD->sICX', 'bnUSD', 'sICX', {
+      price: 1.32,
+    });
     // bnUSD/BALN
-    graph.addDirectedEdgeWithKey('BALN->bnUSD', 'BALN', 'bnUSD', { price: 0.77 });
-    graph.addDirectedEdgeWithKey('bnUSD->BALN', 'bnUSD', 'BALN', { price: 1.28 });
+    graph.addDirectedEdgeWithKey('BALN->bnUSD', 'BALN', 'bnUSD', {
+      price: 0.77,
+    });
+    graph.addDirectedEdgeWithKey('bnUSD->BALN', 'bnUSD', 'BALN', {
+      price: 1.28,
+    });
     // bnUSD/IUSDC
-    graph.addDirectedEdgeWithKey('bnUSD->IUSDC', 'bnUSD', 'IUSDC', { price: 1.04 });
-    graph.addDirectedEdgeWithKey('IUSDC->bnUSD', 'IUSDC', 'bnUSD', { price: 1.28 });
+    graph.addDirectedEdgeWithKey('bnUSD->IUSDC', 'bnUSD', 'IUSDC', {
+      price: 1.04,
+    });
+    graph.addDirectedEdgeWithKey('IUSDC->bnUSD', 'IUSDC', 'bnUSD', {
+      price: 1.28,
+    });
 
-    graph.addDirectedEdgeWithKey('IUSDC->sICX', 'IUSDC', 'sICX', { price: 1.26 });
-    graph.addDirectedEdgeWithKey('sICX->IUSDC', 'sICX', 'IUSDC', { price: 0.77 });
+    graph.addDirectedEdgeWithKey('IUSDC->sICX', 'IUSDC', 'sICX', {
+      price: 1.26,
+    });
+    graph.addDirectedEdgeWithKey('sICX->IUSDC', 'sICX', 'IUSDC', {
+      price: 0.77,
+    });
     console.log(graph.size);
     console.log(graph.toJSON());
     dfs(graph, function (node, attr, depth) {
       console.log(node, attr, depth);
     });
+    const cycles = allSimplePaths(graph, 'sICX', 'sICX');
+    console.log(cycles);
     // const graph = new Graph<NodeType>((n: NodeType) => n.name);
     // graph.insert({ name: 'sICX' });
     // console.log(graph.getNodes());
