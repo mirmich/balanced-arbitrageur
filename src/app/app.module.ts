ea9cf51023ce05f56a92833cdb538886895b1dd2
mirmich <michalem95@gmail.com>
2022-07-02 17:38:50 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { PairListComponent } from './pair-list/pair-list.component';
 import { BotBarComponent } from './bot-bar/bot-bar.component';
 import { TopTradesComponent } from './top-trades/top-trades.component';
 import { EdgeGroomingPipePipe } from './top-trades/edge-grooming-pipe.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   IgxDropDownModule,
   IgxButtonModule,
@@ -18,6 +19,7 @@ import {
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
