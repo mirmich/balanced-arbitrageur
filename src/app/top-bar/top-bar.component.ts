aee588d56953a2e0e2957acf5c48acc293bcb5a6
mirmich <michalem95@gmail.com>
2022-07-06 20:15:53 +0200

    2 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
 export class TopBarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSignIn() {
+    console.log('Sign in called');
+    const customEvent = new CustomEvent('ICONEX_RELAY_REQUEST', {
+      detail: {
+        type: 'REQUEST_ADDRESS',
+      },
+    });
+    const eventHandler = (event) => {
+      const { type, payload } = event.detail;
+      if (type === 'RESPONSE_ADDRESS') {
+        console.log(payload); // e.g., hx19870922...
+      }
+    };
+    window.addEventListener('ICONEX_RELAY_RESPONSE', eventHandler);
+    window.dispatchEvent(customEvent);
   }
-
 }
