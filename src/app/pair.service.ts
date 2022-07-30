ae5f5f2c0cf98e47bd19634781b731b4b1b49636
mirmich <michalem95@gmail.com>
2022-07-30 22:13:35 +0200

    2 files modified

--- src/app/pair.service.ts
+++ src/app/pair.service.ts
@@ -180,3 +180,45 @@ export class PairService {
     // );
   }
 }
+/**
+ * 
+ * {
+ * SWAP bnUSD to sICX
+    "to": "cx88fd7df7ddff82f7cc735c871dc519838cb235bb", recipient is bnUSD contract
+    "from": "hx97180db9263685f07bed00df5111481513ab30c1", me
+    "nid": "0x1", idk
+    "version": "0x3", idk
+    "timestamp": "0x5e50abe017af8",
+    "stepLimit": "0x42c1d80",
+    "value": "0x0",
+    "dataType": "call",
+    "data": {
+        "method": "transfer",
+        "params": {
+            "_to": "cx21e94c08c03daee80c25d8ee3ea22a20786ec231", balanced router contract
+            "_value": "0x1812482108e2eeb", amount probably
+            "_data": "0x7b226d6574686f64223a225f73776170222c22706172616d73223a7b22746f546f6b656e223a22637832363039623932346533336566303062363438613430393234356337656133393463343637383234222c226d696e696d756d52656365697665223a22323939313134363231373733363937393839222c2270617468223a5b22637833613336656131663662396161336432646439636236386538393837626363336161626161613838222c22637832363039623932346533336566303062363438613430393234356337656133393463343637383234225d7d7d"
+        }
+    }
+}
+
+Stability fund
+{
+    "to": "cx88fd7df7ddff82f7cc735c871dc519838cb235bb",
+    "from": "hx97180db9263685f07bed00df5111481513ab30c1",
+    "nid": "0x1",
+    "version": "0x3",
+    "timestamp": "0x5e50b3354e948",
+    "stepLimit": "0x42c1d80",
+    "value": "0x0",
+    "dataType": "call",
+    "data": {
+        "method": "transfer",
+        "params": {
+            "_to": "cxa09dbb60dcb62fffbd232b6eae132d730a2aafa6",
+            "_value": "0x1812482108e2eeb",
+            "_data": "0x637862623238373166343638613330303866383062303866646465356238623935313538336163663036"
+        }
+    }
+}
+ */
