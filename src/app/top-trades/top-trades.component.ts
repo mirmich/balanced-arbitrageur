e77a322bde346ac64806b7caf690d336bfae496e
mirmich <39803741+mirmich@users.noreply.github.com>
2023-02-25 19:44:02 +0100

    1 file modified

--- src/app/top-trades/top-trades.component.ts
+++ src/app/top-trades/top-trades.component.ts
@@ -24,13 +24,14 @@ export class TopTradesComponent implements OnDestroy {
     );
   }
   public trade(): void {
-    this.pairService.doTrade2(
+    const result = this.pairService.doTrade2(
       'hx97180db9263685f07bed00df5111481513ab30c1',
       'cxbb2871f468a3008f80b08fdde5b8b951583acf06',
       'cx88fd7df7ddff82f7cc735c871dc519838cb235bb',
       '113335621541758528',
       ['cx88fd7df7ddff82f7cc735c871dc519838cb235bb']
     );
+    console.log(result);
   }
 
   public ngOnDestroy(): void {
