20fc8679f7b3e7b581bd2fa00789abfe0d8ff780
mirmich <39803741+mirmich@users.noreply.github.com>
2023-04-02 20:59:21 +0200

    2 files modified

--- src/app/top-trades/top-trades.component.ts
+++ src/app/top-trades/top-trades.component.ts
@@ -13,6 +13,8 @@ import { ArtbitraguePath } from './model';
 })
 export class TopTradesComponent implements OnDestroy {
   private readonly arbitragueSubscription: Subscription;
+  private readonly balancedRouterContract: string =
+    'cx21e94c08c03daee80c25d8ee3ea22a20786ec231';
 
   public arbitragues: Array<ArtbitraguePath> = [];
   constructor(
@@ -28,11 +30,12 @@ export class TopTradesComponent implements OnDestroy {
     );
   }
   // cxbb2871f468a3008f80b08fdde5b8b951583acf06
-  public trade(): void {
+  public trade(index: number): void {
+    console.log(index);
     const result = this.tradeService.doTradeRPC(
-      'cx21e94c08c03daee80c25d8ee3ea22a20786ec231',
-      'cx88fd7df7ddff82f7cc735c871dc519838cb235bb',
-      'cxbb2871f468a3008f80b08fdde5b8b951583acf06',
+      this.balancedRouterContract,
+      this.arbitragues[index].cycle[0].tokenFromContract,
+      this.arbitragues[index].cycle[0].tokenFromContract,
       '92022965438856284', // Needs to be divided by 100000000000000000 to know the actual amount, the number will depend on token/pool
       ['cxbb2871f468a3008f80b08fdde5b8b951583acf06']
     );
